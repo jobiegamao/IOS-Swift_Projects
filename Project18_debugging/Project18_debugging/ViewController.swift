975f42e08b22ae8c8cf5bdd75f816396225c2c48
Jobie Gamao <jobiemaygamao@gmail.com>
2022-12-24 00:32:12 +0800

    proj19 done

---
+++ Project18_debugging/Project18_debugging/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Project18_debugging
+//
+//  Created by may on 12/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		for i in 1 ... 100 {
+			print("Got number \(i)")
+		}
+	}
+
+
+}
+
